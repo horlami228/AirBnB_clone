b1947ee89459db3165ad6e99682cc90ebedd40f1
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-09 22:54:09 +0100

    updated all

---
+++ generate_author_list.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This Bash script is used to generate Authors list to this project
+git shortlog -se \
+  | perl -spe 's/^\s+\d+\s+//' \
+  | sed -e '/^CommitSyncScript.*$/d' \
+  > AUTHORS
