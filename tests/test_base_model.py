a8422454ddb67e40eda8c24437970110bfb6c10d
Theophilus Samuel <psalmomo@gmail.com>
2023-08-10 03:18:04 +0100

    updates

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,15 +1,41 @@
+"""
+This script demonstrates the usage of the BaseModel class and its methods.
+"""
+
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
+try:
+    # Create an instance of the BaseModel class
+    my_model = BaseModel()
+
+    # Set the name attribute of the model
+    my_model.name = "My First Model"
+
+    # Set the my_number attribute of the model
+    my_model.my_number = 89
+
+    # Print the model object
+    print(my_model)
+
+    # Save the model to a file
+    my_model.save()
+
+    # Print the updated model object after saving
+    print(my_model)
+
+    # Convert the model object to a dictionary
+    my_model_json = my_model.to_dict()
+
+    # Print the JSON representation of the model
+    print(my_model_json)
+
+    # Print the individual key-value pairs of the JSON representation
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key,
+              type(my_model_json[key]), my_model_json[key]))
+
+except Exception as e:
+    # Handle any exceptions that occur during execution
+    print(f"An error occurred: {e}")
