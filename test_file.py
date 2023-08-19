5fad62bad28ca06a934204e90d063ec088ece713
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-19 14:41:05 +0100

    untracked

--- test_file.py
+++ test_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
@@ -9,8 +11,14 @@ for obj_id in all_objs.keys():
     print(obj)
 
 print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+user = User()
+user.first_name = "olamilekan"
+user.last_name = "akintola"
+user.email = "akintola14@gmail.com"
+
+user.save()
+
+place = Place()
+place.user_id
+
+
